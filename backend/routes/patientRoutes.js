e5bd4d7d3325d479d7c90d41275e0eaacc6da322
Ameya Shahu <ameyashahu@gmail.com>
2021-04-26 15:29:43 +0530

    list patients endpoint added

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -9,6 +9,7 @@ let PatientRoute = express.Router();
 
 // Patient routes
 
+// Register New Patients
 PatientRoute.post(
     '/register',
     expressAsyncHandler( async (req,res)=>{
@@ -31,4 +32,29 @@ PatientRoute.post(
     })
 );
 
+// List all the Patients
+/* This is paginated endpoint */
+PatientRoute.get(
+    '/',
+    expressAsyncHandler(async (req, res)=>{
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+
+        const skipIndex = (page - 1) * limit;
+
+        const patientList = await Patient.find({})
+            .sort({name:1})
+            .limit(limit)
+            .skip(skipIndex)
+            .select(['name', 'gender']);
+
+        if (patientList.length>0){
+            res.status(200);
+            res.json(patientList);
+        }else{
+            res.status(404);
+            throw new Error(JSON.stringify({"patientsList": "No patient record found on this page"}))
+        }
+    })
+)
 module.exports = PatientRoute;
